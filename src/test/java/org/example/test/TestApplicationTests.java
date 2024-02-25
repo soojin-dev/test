5698d277dcc6598f1707e709552d7c1ca47ca992
soojin-dev <dvxz1125@gmail.com>
2024-02-25 22:07:17 +0900

    initial test

--- src/test/java/org/example/test/TestApplicationTests.java
+++ src/test/java/org/example/test/TestApplicationTests.java
@@ -28,7 +28,7 @@ class TestApplicationTests {
         int result = testApplication.plus(num1, num2);
 
         // then
-        assertThat(result).isEqualTo(4);
+        assertThat(result).isEqualTo(3);
     }
 
 }
