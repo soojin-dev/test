ebceee446ffeabcd2c41cc8df8b2e5d9a18df51d
soojin-dev <dvxz1125@gmail.com>
2024-02-25 20:43:47 +0900

    initial test

--- src/main/java/org/example/test/TestApplication.java
+++ src/main/java/org/example/test/TestApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+
+
+    }
+
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
 
 }
--- src/test/java/org/example/test/TestApplicationTests.java
+++ src/test/java/org/example/test/TestApplicationTests.java
@@ -1,13 +1,33 @@
 package org.example.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    TestApplication testApplication;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @DisplayName("덧셈의 결과를 조회한다.")
+    @Test
+    void test() {
+        // given
+        int num1 = 1;
+        int num2 = 3;
+
+        // when
+        int result = testApplication.plus(num1, num2);
+        // then
+        assertThat(result).isEqualTo(4);
+    }
 
 }
